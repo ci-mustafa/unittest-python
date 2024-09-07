4fab6f4c32e013ee8908e7f1f8af5a90830936ae
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-09-07 10:48:28 +0000

    feat: raise TypeError if value passed to function is not a list

--- test_evens.py
+++ test_evens.py
@@ -1,8 +1,12 @@
 import unittest
+from evens import even_number_of_evens
 
 
 class TestEvens(unittest.TestCase):
-    pass
+    def test_thowes_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 5)
+        self.assertRaises(TypeError, even_number_of_evens, "mustafa")
+    
 
 
 
